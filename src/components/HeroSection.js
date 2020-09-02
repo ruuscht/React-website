633145a359eb77d8539fc2eb9371cc30d0ac131b
Rouza mousa <rouza_124@live.se>
2020-09-02 18:01:50 +0200

    Färdig React hemsida

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import "../App.css";
+
+function HoroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video.mp4" autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          BOOK NOW <i class="fas fa-plane-departure" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HoroSection;
